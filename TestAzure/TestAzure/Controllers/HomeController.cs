12e75193433aed1fe2dece50e1f5df70579bd467
Practice-KhongTen <59854810+Practice-KhongTen@users.noreply.github.com>
2021-01-08 20:35:59 +0700

    chanhoa

---
+++ TestAzure/TestAzure/Controllers/HomeController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using TestAzure.Models;
+
+
+namespace TestAzure.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        private TAzureRepository repository;
+        public int PageSize = 4;
+        public HomeController(TAzureRepository repo)
+        {
+            repository = repo;
+        }
+
+        public IActionResult Index() => View(repository.Products);
+        //private readonly ILogger<HomeController> _logger;
+
+        //public HomeController(ILogger<HomeController> logger)
+        //{
+        //    _logger = logger;
+        //}
+
+        //public IActionResult Index()
+        //{
+        //    return View();
+        //}
+
+        //public IActionResult Privacy()
+        //{
+        //    return View();
+        //}
+
+        //[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        //public IActionResult Error()
+        //{
+        //    return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        //}
+    }
+}
